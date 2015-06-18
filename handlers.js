c8e90691e298341e63484143b29ba844bfeb3807
rjmk <rafekennedy@gmail.com>
2015-06-18 21:00:03 +0100

    stuff not merged

--- handlers.js
+++ handlers.js
@@ -7,7 +7,7 @@ module.exports = {
       reply.file('public/index.html');
     },
 
-    'GET /{picture}' : function(request, reply){
+    'GET /view/{picture}' : function(request, reply){
       reply('<img src="https://s3.amazonaws.com/polagraph/' + request.params.picture + '">');
     },
 
