815f9c04b7ee139882076accc03442bdfb741236
Anni Vaananen <anni.vaananen@gmail.com>
2015-06-18 15:44:18 +0100

    details saved to profile!

--- routes.js
+++ routes.js
@@ -14,7 +14,17 @@ module.exports = [
         auth:'google',
             handler: function(request, reply){
                 var creds = request.auth.credentials;
-                console.log('creds', creds);
+                //console.log('creds', creds);
+
+                var profile = {
+                    googleId: creds.profile.id,
+                    fullName: creds.profile.displayName,
+                    firstName: creds.profile.name.first,
+                    email: creds.profile.email,
+                };
+
+                console.log('profile', profile);
+
                 request.auth.session.clear();
                 request.auth.session.set({googleName:creds.profile.displayName});
                 //request.auth.session.set(request.auth.credentials.profile); 
