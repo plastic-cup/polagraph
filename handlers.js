1fdfe09bf6db30135cc0d7c12290aeca4be3c9c6
Danielwhyte <danwhy@gmail.com>
2015-06-19 08:19:15 +0100

    pictures and email working

--- handlers.js
+++ handlers.js
@@ -1,6 +1,10 @@
 var AWS = require('aws-sdk');
 var filetype = require('file-type');
 var s3 = new AWS.S3();
+var mandrill = require("mandrill-api/mandrill");
+var mandrill_client = new mandrill.Mandrill(process.env.SECRET);
+var mongo = require('./mongo');
+
 
 module.exports = {
     'GET /' : function(request, reply){
@@ -18,6 +22,9 @@ module.exports = {
             Key : request.payload.title + '.' + type.ext,
             Body : request.payload.upload,
             ContentType : type.mime,
+            Metadata : {
+                'uploadtime' : new Date().getTime().toString()
+            }
         }, function(err, data){
             console.log(err);
             console.log(data);
@@ -28,5 +35,58 @@ module.exports = {
         directory: {
             path: 'public',
         }
+    },
+
+    login : function(request, reply){
+        var creds = request.auth.credentials;
+
+        var profile = {
+            googleId: creds.profile.id,
+            fullName: creds.profile.displayName,
+            firstName: creds.profile.name.first,
+            email: creds.profile.email,
+            pictures : []
+        };
+
+        mongo.read('users', profile, function(err, data){
+            if (data.length === 0){
+                mongo.insert('users', profile, function(err, data){
+                    console.log(data);
+                    sendEmail(data[0].email, data.firstName);
+                });
+            }
+        });
+
+        request.auth.session.clear();
+        request.auth.session.set({googleName:creds.profile.displayName});
+        reply.file('feed.html');
+    },
+
+    'GET /all' : function(request, reply){
+        s3.listObjects({Bucket : 'polagraph'}, function(err, data){
+            console.log(err);
+            reply(data.Contents);
+        });
     }
 };
+
+function sendEmail(email, name) {
+	var data = {
+	      	'from_email': 'danwhy@gmail.com',
+	      	'to': [
+	          {
+	            'email': email,
+	            'name': name,
+	            'type': 'to'
+	          }
+	        ],
+		      'autotext': 'true',
+		      'subject': 'Welcome to Polagraph',
+		      'html': '<p>Hi ' + name +', welcome to Polagraph</p>'
+	};
+	mandrill_client.messages.send({"message": data, "async": false},function(result) {
+		console.log(result);
+	}, function(e) {
+		console.log("Error " + e.message);
+	});
+}
--- routes.js
+++ routes.js
@@ -13,30 +13,7 @@ module.exports = [
     path : '/login',
     config: {
         auth:'google',
-            handler: function(request, reply){
-                var creds = request.auth.credentials;
-                //console.log('creds', creds);
-
-                var profile = {
-                    googleId: creds.profile.id,
-                    fullName: creds.profile.displayName,
-                    firstName: creds.profile.name.first,
-                    email: creds.profile.email,
-                };
-
-                mongo.read('users', profile, function(err, data){
-                    if (data.length === 0){
-                        mongo.insert('users', profile, function(err, data){
-                            console.log(data);
-                        });
-                    }
-                });
-
-                request.auth.session.clear();
-                request.auth.session.set({googleName:creds.profile.displayName});
-                //request.auth.session.set(request.auth.credentials.profile);
-            reply.file('feed.html');
-            }
+            handler: handlers['login']
         }
     },
     {
@@ -90,6 +67,12 @@ module.exports = [
         method: 'GET',
         path: '/static/{path*}',
         handler: handlers['GET /static/{path*}']
+    },
+
+    {
+        method: 'GET',
+        path: '/all',
+        handler: handlers['GET /all']
     }
 
 ];
